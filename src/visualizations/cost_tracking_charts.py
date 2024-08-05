6ece883fe796e867bd96d0fa564d788d642bced7
junior <junior@edenfarm.id>
2024-08-06 03:19:20 +0700

    chore: remove unused file

--- src/visualizations/cost_tracking_charts.py
+++ src/visualizations/cost_tracking_charts.py
@@ -14,18 +14,39 @@ def create_cost_table(df):
     formatted_df = df.copy()
     formatted_df['date'] = pd.to_datetime(formatted_df['date']).dt.strftime('%Y-%m-%d')
     formatted_df['amount'] = formatted_df['amount'].apply(lambda x: f"${x:,.2f}")
-    
+
     if df.empty:
         return {}
+
+    total_amount = df['amount'].sum()
+    total_row = ['Total Amount', f"${total_amount:,.2f}", '', '', '', '', '', '']
+
+    if len(total_row) < len(formatted_df.columns):
+        total_row.extend([''] * (len(formatted_df.columns) - len(total_row)))
+
+    formatted_df.loc[len(formatted_df)] = total_row
+
     fig = go.Figure(data=[go.Table(
         header=dict(values=list(formatted_df.columns),
                     fill_color='paleturquoise',
                     align='left'),
         cells=dict(values=[formatted_df[col] for col in formatted_df.columns],
-                   fill_color='lavender',
-                   align='left'))
-    ])
+                   fill_color=[['lavender'] * (len(formatted_df) - 1) + ['lightyellow']],
+                   align='left',
+                   height=30)
+    )])
+
+    fig.update_layout(
+        autosize=True,
+        margin=dict(l=0, r=0, t=0, b=0),
+        height=400,
+    )
+    
+    fig.update_traces(
+        columnwidth=[150, 60, 70, 70, 40, 40, 40, 40, 100, 50],
+    )
     return fig
+
 
 def create_cost_summary(df):
     if df.empty:
